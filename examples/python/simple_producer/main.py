460b21f75297c63d3b0461781b3ac0fb0cd898c6
Ondrej Sika <ondrej@ondrejsika.com>
2026-06-24 23:35:02 +0200

    feat(examples/python/simple_producer): Add simple producer example in Python

---
+++ examples/python/simple_producer/main.py
@@ -0,0 +1,23 @@
+from confluent_kafka import Producer
+import time
+
+BROKER_ADDR = "127.0.0.1:9092"
+TOPIC = "simple"
+
+
+def main():
+    p = Producer({"bootstrap.servers": BROKER_ADDR})
+
+    i = 0
+    while True:
+        key = str(i)
+        msg = f"simple_py_{i}"
+        p.produce(TOPIC, key=key, value=msg)
+        p.flush()
+        print(f"produced: key={key} msg={msg}")
+        i += 1
+        time.sleep(1)
+
+
+if __name__ == "__main__":
+    main()
